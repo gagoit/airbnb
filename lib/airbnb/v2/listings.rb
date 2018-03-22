46e0db97d676853b8bb5b63523537ef2ddd527b2
Dat Pham-Ba <dat_phamba1307@yahoo.com>
2018-03-22 16:10:13 +0700

    Add search listings, get reviews of a listing api

--- lib/airbnb/v2/listings.rb
+++ lib/airbnb/v2/listings.rb
@@ -1,8 +1,75 @@
 module Airbnb
   class V2::Listings < Base
     class << self
+      ##
+      # URL: https://api.airbnb.com/v2/listings/:id
+      # Params: 
+      #  [Required] client_id:
+      #  [Required] _format:
+      # Response:
+      # {
+      #   search_results: [
+      #     {
+      #       listing: {},
+      #       pricing_quote: {},
+      #       viewed_at: null
+      #     }    
+      #   ]
+      # }
+      ##
       def show(id)
         response = HTTP.get("#{Airbnb::V2::URL}/listings/#{id}", params: { _format: 'v1_legacy_for_p3', client_id: client_id })
+        JSON.parse(response.to_str)
+      end
+      
+      ##
+      # URL: https://api.airbnb.com/v2/search_results
+      # Params: 
+      #  [Required] client_id:
+      # Response:
+      # {
+      #   search_results: [
+      #     {
+      #       listing: {},
+      #       pricing_quote: {},
+      #       viewed_at: null
+      #     }    
+      #   ]
+      # }
+      ##
+      def search(query)
+        params = { client_id: client_id }
+        params.merge!(query)
+        response = HTTP.get("#{Airbnb::V2::URL}/search_results", params: params)
+        JSON.parse(response.to_str)
+      end
+      
+      ##
+      # URL: https://api.airbnb.com/v2/reviews
+      # Params: 
+      #  [Required] client_id:
+      #  [Required] role: 'all'
+      #  [Required] listing_id:
+      # Response:
+      # {
+      #   reviews: [
+      #     {
+      #       author: {},
+      #       author_id: 152062791,
+      #       can_be_edited: false,
+      #       comments: "abc", 
+      #       created_at: "2018-02-17T22:33:32Z",
+      #       id: 235725728,
+      #       listing_id: 2056659,
+      #       recipient_id: 10457593
+      #     }    
+      #   ]
+      # }
+      ##
+      def reviews(id, query)
+        params = { client_id: client_id, role: 'all', listing_id: id }
+        params.merge!(query)
+        response = HTTP.get("#{Airbnb::V2::URL}/reviews", params: params)
         JSON.parse(response.to_str)
       end
     end
