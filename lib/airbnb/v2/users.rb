72efeb5492cf46b89d49d5b6955c9a7a9a8d103b
Dat Pham-Ba <dat_phamba1307@yahoo.com>
2018-03-22 16:20:53 +0700

    Add get host listings of user

--- lib/airbnb/v2/users.rb
+++ lib/airbnb/v2/users.rb
@@ -1,8 +1,46 @@
 module Airbnb
   class V2::Users < Base
     class << self
+      ##
+      # URL: https://api.airbnb.com/v2/users/:id
+      # Params: 
+      #  [Required] client_id:
+      #  [Required] _format:
+      # Response:
+      # {
+      #   user: {
+      #     
+      #   },
+      #   metadata: { }
+      # }
+      ##
       def show(id)
-        response = HTTP.get("#{Airbnb::V2::URL}/users/#{id}", params: { client_id: client_id })
+        response = HTTP.get("#{Airbnb::V2::URL}/users/#{id}", params: { _format: 'v1_legacy_show', client_id: client_id })
+        JSON.parse(response.to_str)
+      end
+      
+      ##
+      # Returns information about all the listings a user hosts.
+      # URL: https://api.airbnb.com/v2/listings
+      # Params: 
+      #  [Required] client_id:
+      #  user_id: 
+      # Response:
+      # {
+      #   listings: [
+      #     {
+      #        ...
+      #        user: {}
+      #     }
+      #   ],
+      #   metadata: {
+      #     listing_count: 1,
+      #     record_count: 1
+      #   }
+      # }
+      ##
+      def host_listings(id)
+        response = HTTP.get("#{Airbnb::V2::URL}/listings", params: { client_id: client_id, user_id: id })
         JSON.parse(response.to_str)
       end
     end
